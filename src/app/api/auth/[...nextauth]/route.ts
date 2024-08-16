a7f07619888fa7ff42cbc46ae548ada8f7b652ce
Ahmed Drioueche <adsrahmed@gmail.com>
2024-08-16 18:18:35 +0100

    update

---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,43 @@
+import NextAuth from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+
+const authHandler = NextAuth({
+    providers: [
+        GoogleProvider({
+            clientId: process.env.GOOGLE_CLIENT_ID!,
+            clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+        }),
+    ],
+    pages: {
+        signIn: '/auth/signup', 
+        error: '/auth/error',
+    },
+    session: {
+        strategy: 'jwt',
+    },
+    callbacks: {
+        async jwt({ token, user }) {
+            if (user) {
+                token.id = user.id as string;
+                token.email = user.email as string;
+                token.name = user.name as string | undefined;
+                token.image = user.image as string | undefined;
+            }
+            return token;
+        },
+        async session({ session, token }) {
+            if (token) {
+                session.user = {
+                    id: token.id as string,
+                    email: token.email as string,
+                    name: token.name as string | undefined,
+                    image: token.image as string | undefined
+                };
+            }
+            return session;
+        }
+    }
+});
+
+export const GET = authHandler;
+export const POST = authHandler;

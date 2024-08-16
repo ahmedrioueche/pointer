8ba0c1e00563b1ae25d46edd588c95d2c2821e41
Ahmed Drioueche <adsrahmed@gmail.com>
2024-08-16 18:52:37 +0100

    update

--- src/app/(pages)/auth/error/page.tsx
+++ src/app/(pages)/auth/error/page.tsx
@@ -10,14 +10,14 @@ const ErrorPage: React.FC = () => {
             <div className="container mx-auto flex flex-col items-center">
                 <div className="relative md:w-1/2 flex flex-col items-center bg-white dark:bg-dark-background rounded-lg shadow-lg p-8 font-stix">
                     <FaExclamationTriangle className="text-5xl mb-4 text-red-500 dark:text-red-400" />
-                    <h2 className={`text-3xl font-bold mb-4 bg-gradient-to-r from-dark-primary to-dark-accent text-transparent bg-clip-text`}>
+                    <h2 className={`text-2xl font-bold mb-4 bg-gradient-to-r from-dark-primary to-dark-accent text-transparent bg-clip-text`}>
                         Something Went Wrong
                     </h2>
                     <p className={`text-lg mb-6 dark:text-dark-text text-light-text'}`}>
                         An error occurred while trying to process your request. Please try again later or contact support if the issue persists.
                     </p>
                     <Link href="/" className="w-full px-6 py-3 rounded-md bg-light-primary dark:bg-dark-primary text-white font-medium flex items-center justify-center gap-2 transition-colors duration-100 hover:bg-blue-600">
-                        <span>Go to Home</span>
+                        <span>Go Home</span>
                     </Link>
                 </div>
             </div>

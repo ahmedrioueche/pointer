831ee65cc09f636bd360b510dfeba595256b2b7e
Ahmed Drioueche <adsrahmed@gmail.com>
2024-08-16 18:43:55 +0100

    update

--- src/app/(pages)/auth/error/page.tsx
+++ src/app/(pages)/auth/error/page.tsx
@@ -13,7 +13,7 @@ const ErrorPage: React.FC = () => {
                     <h2 className={`text-3xl font-bold mb-4 bg-gradient-to-r from-dark-primary to-dark-accent text-transparent bg-clip-text`}>
                         Something Went Wrong
                     </h2>
-                    <p className={`text-lg mb-6 ${document.documentElement.classList.contains('dark') ? 'text-dark-text' : 'text-light-text'}`}>
+                    <p className={`text-lg mb-6 dark:text-dark-text text-light-text'}`}>
                         An error occurred while trying to process your request. Please try again later or contact support if the issue persists.
                     </p>
                     <Link href="/" className="w-full px-6 py-3 rounded-md bg-light-primary dark:bg-dark-primary text-white font-medium flex items-center justify-center gap-2 transition-colors duration-100 hover:bg-blue-600">

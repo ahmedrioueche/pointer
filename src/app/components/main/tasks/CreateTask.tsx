afa212413e5fdf3950b98d7dd42a95842b3e239c
Ahmed Drioueche <adsrahmed@gmail.com>
2024-08-22 20:46:08 +0100

    update

--- src/app/components/main/tasks/CreateTask.tsx
+++ src/app/components/main/tasks/CreateTask.tsx
@@ -64,6 +64,7 @@ export const CreateTask: React.FC<CreateTaskProps> = ({ type, onCreate }) => {
         <input
           type="datetime-local"
           value={newTaskDate}
+          placeholder="Select date and time"
           onChange={(e) => setNewTaskDate(e.target.value)}
           className="flex-1 p-4 rounded-lg mt-3 bg-indigo-400 border border-indigo-400 focus:border-dark-background dark:focus:border-light-background text-light-text dark:text-dark-text placeholder:text-light-text dark:placeholder-dark-text transition-all duration-300 focus:outline-none box-border"
           style={{
